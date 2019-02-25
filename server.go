84dc7d4dc57614011d41502607c35d432e6e7d20
nakapi <nakapi07@gmail.com>
2019-02-25 21:48:11 +0900

    elasticSearch 暫定

--- server.go
+++ server.go
@@ -4,6 +4,9 @@ package main
 
 //  インポートパッケージ
 import (
+	"app/domain"
+	"app/infrastructure/config/json"
+	"app/infrastructure/database"
 	"app/infrastructure/dicontainer"
 	"app/interface/controller"
 	"app/interface/logger"
@@ -36,6 +39,25 @@ func main() {
 
 // メイン処理の中で、実際の処理を担い結果を出力します。
 func (client *CLI) Run(args []string) int {
+
+	// ElasticSearch
+	config, err := json.NewConfigHandler()
+	if err != nil {
+		fmt.Println("Config Failed", err.Error())
+	}
+	elclient, err := database.NewSqlHandler(config)
+	if err != nil {
+		fmt.Println("Elastic Client Failed", err.Error())
+	}
+	result, err := elclient.Query("id", "2")
+	if err != nil {
+		fmt.Println("Elastic Query Failed", err.Error())
+	}
+	var testObject domain.Test
+	result.Scan(&testObject)
+	fmt.Println(testObject)
+
+	return noError
 
 	// DIContainer
 	container, err := dicontainer.NewContainerHandler()
